8d2fd8b8561ba2c215c436129de7d606cd7b8848
dydgh142 <98027471+dydgh142@users.noreply.github.com>
2023-02-20 18:20:53 +0900

    react 설명추가

--- react.js
+++ react.js
@@ -1,4 +1,5 @@
 class Counter extends React.Component {
+  //기본값 설정 
   static defaultProps = {
     min: 0,
     count: 0,
@@ -6,11 +7,14 @@ class Counter extends React.Component {
     step: 1,
   };
 
+  //count의 기본값 설정 
   state = {
     count: this.props.count,
   };
 
+  //랜더링
   render() {
+    //선택된 element의 값들을 가져옴
     const { min, max } = this.props;
     const { count } = this.state;
 
@@ -37,6 +41,7 @@ class Counter extends React.Component {
     );
   }
 
+  //증가버튼을 눌렀을때 해당 함수가 실행됨.
   handleIncrement = () => {
     const { max, step } = this.props; 
     let newCountValue = this.state.count + step;
@@ -48,6 +53,7 @@ class Counter extends React.Component {
     });
   };
 
+  //감소버튼을 눌렀을때 해당 함수가 실행됨
   handleDecrement = () => {
     const { min, step } = this.props; 
     let newCountValue = this.state.count - step;
@@ -60,6 +66,7 @@ class Counter extends React.Component {
   };
 }
 
+//Counter라는 <div> 요소를 가져와 가상돔을 생성함. 첫번째 것은 설정값이 없고 두번짼 7,9,16,4 등의 값이 설정됨.
 const counterContainer = document.querySelector('[data-component="Counter"]');
 const reactDomRoot = ReactDOM.createRoot(counterContainer);
 reactDomRoot.render(
